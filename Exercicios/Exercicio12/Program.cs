5f6486f845aa541f82b146e9547625adc7b7ca30
João Paulo <joaopaulocostar@outlook.com>
2025-11-23 15:20:05 -0300

    Exercicio12

---
+++ Exercicios/Exercicio12/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+public class Exercicio12
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Informe a tempera de graus celsios: C°");
+        double celsios = Convert.ToDouble(Console.ReadLine());
+        double f = CalculadoraTemperatura(celsios);
+        Console.WriteLine($"A temperatura de C°{celsios} é igual a F°{f}");
+    }
+    public static double CalculadoraTemperatura(double c)
+    {
+        return ((9 * c) /5) + 32;
+    }
+}
