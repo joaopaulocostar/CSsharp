6d55484a1d01175a6b9ea5f215f7968ac0875152
João Paulo <joaopaulocostar@outlook.com>
2025-12-20 11:32:26 -0300

    Exercicio14

---
+++ Exercicios/Exercicio14/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Exercicio14{
+    public static void Main(){
+        Console.WriteLine("====== Hipotenusa ======");
+        Console.WriteLine("Informe o valor do Cateto: ");
+        double cateto = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Informe o valor do Adjacente: ");
+        double adjacente = Convert.ToDouble(Console.ReadLine());
+        double hipotenusa = Hipotenusa(cateto, adjacente);
+        Console.WriteLine($"O valor da hipotenusa é {hipotenusa}");
+
+
+    }
+    static double Hipotenusa(double a, double b){
+    return Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+    }
+}
