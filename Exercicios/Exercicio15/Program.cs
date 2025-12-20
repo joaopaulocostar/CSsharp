1139619fb92389130416aac9e5515f43307c397b
João Paulo <joaopaulocostar@outlook.com>
2025-12-20 13:11:03 -0300

    Exercicio15 e inicio do 16

---
+++ Exercicios/Exercicio15/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+class Exercicio15
+{
+    public static void Main()
+    {
+        List<string> alunos = new List<string>();
+        string entrada;
+        Console.WriteLine("Escreva o nome do Aluno:(Escreva FIM para encerrar.) -> ");
+        while (true)
+        {
+            entrada = Console.ReadLine();
+            if(entrada.ToUpper() == "FIM") break;
+            else if (entrada == null) break;
+            else alunos.Add(entrada);
+        }
+
+        string escolhido = Sorteio(alunos);
+        if (escolhido != null) Console.WriteLine($"O aluno sorteado foi {escolhido}");
+        else Console.WriteLine("Nenhum nome inserido para sorteio");
+    }
+
+    static string Sorteio(List<string> Lista)
+    {
+        if (Lista.Count == 0) return null;
+        Random sorteio = new Random();
+        string escolhido = Lista[sorteio.Next(Lista.Count)];
+        return escolhido;
+    }
+    
+        
+    
+}
---
+++ Exercicios/Exercicio16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+class Exercicio16
+{
+    public static void Main()
+    {
+        Console.WriteLine("Informe uma frase: ");
+        string frase = Console.ReadLine();
+        int quantidade = Quantidade(frases);
+        Console.WriteLine($"A quantidade de A na frase é igual {quantidade}");
+    }
+
+    static int Quantidade(string frases)
+    {
+        int quantidade = frases.Count(n => n == "A");
+        return quantidade;
+    }
+}
